06f260c05d33686f3e3d2c6ee04728d82da54a27
mwchoo <cmw1408@u.sogang.ac.kr>
2020-04-19 00:15:00 +0900

    [Modify] material of back wall

--- p4_Minwoo_Choo_20141150/space.js
+++ p4_Minwoo_Choo_20141150/space.js
@@ -3,12 +3,12 @@ function drawSpace() {
   translate(0, -5, -1000);
   fill(150, 200, 100);
 
-  noStroke();
-  // front-back
-  plane(1000, 1000);
-
   // bottom
   texture(textureMat.concrete);
+  noStroke();
+
+  // front-back
+  plane(1000, 1000);
   rotateX(HALF_PI);
   translate(0, 1000, -500);
   plane(1000, 2000);
